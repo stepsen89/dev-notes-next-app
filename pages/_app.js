1a8ad83f1ea7a27cf8024262d019c7b60ee1d426
Steph <stepsen.dev@gmail.com>
2021-10-02 15:16:58 +0100

    fixed fontawesome import and first form approach

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 import Layout from "../components/shared/Layout";
 import "semantic-ui-css/semantic.min.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/new-note.js
+++ pages/new-note.js
@@ -1,5 +1,32 @@
+import { Form, Button } from "semantic-ui-react";
+import classes from "../styles/Form.module.css";
+
+const dummy = {
+  id: 1,
+  header: "Project Report - April",
+  description:
+    "Leverage agile frameworks to provide a robust synopsis for high level overviews.",
+  meta: "ROI: 30%",
+};
+
 function NewNote() {
-  return <div> Hello </div>;
+  return (
+    <Form className={classes.form}>
+      <Form.Field>
+        <label>Title</label>
+        <input />
+      </Form.Field>
+      <Form.Field>
+        <label>Description</label>
+        <input placeholder='Last Name' />
+      </Form.Field>
+      <Form.Field>
+        <label>Link</label>
+        <input placeholder='Last Name' />
+      </Form.Field>
+      <Button type='submit'>Submit</Button>
+    </Form>
+  );
 }
 
 export default NewNote;
