09fb004798dc6cf2bbbb5482bb7648f71764efaf
Steph <stepsen.dev@gmail.com>
2021-10-02 13:53:46 +0100

    added semantic ui css and react

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Layout from "../components/shared/Layout";
+import "semantic-ui-css/semantic.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (
